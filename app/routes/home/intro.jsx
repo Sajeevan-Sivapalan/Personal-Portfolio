ea95f99d261a92e01f14b4b3c903b14d45d668a6
IT21204302 <99412150+IT21204302@users.noreply.github.com>
2024-08-07 09:03:33 +0530

    path test

--- app/routes/home/intro.jsx
+++ app/routes/home/intro.jsx
@@ -14,7 +14,7 @@ import { useHydrated } from '~/hooks/useHydrated';
 import styles from './intro.module.css';
 
 const DisplacementSphere = lazy(() =>
-  import('./displacement-sphere').then(module => ({ default: module.DisplacementSphere }))
+  import('~/routes/home/displacement-sphere').then(module => ({ default: module.DisplacementSphere }))
 );
 
 export function Intro({ id, sectionRef, scrollIndicatorHidden, ...rest }) {
