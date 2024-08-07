ce34033c06190b0cbcdb74c8ac66beff3dd37923
IT21204302 <99412150+IT21204302@users.noreply.github.com>
2024-08-07 09:11:06 +0530

    path revert

--- app/routes/home/project-summary.jsx
+++ app/routes/home/project-summary.jsx
@@ -1,4 +1,4 @@
-import { Button } from './button/button';
+import { Button } from '~/components/button';
 import { Divider } from '~/components/divider';
 import { Heading } from '~/components/heading';
 import { deviceModels } from '~/components/model/device-models';
